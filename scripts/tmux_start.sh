b99283bbbc4323c3b562e56b8aa4406b1f8d6374
Michael Häußlein <michael@haeusslein.de>
2020-10-21 09:53:30 +0200

    waybar update

--- scripts/tmux_start.sh
+++ scripts/tmux_start.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
 SESSIONNAME="base"
 WINDOW_1='main'
-WINDOW_2="database"
+WINDOW_2="otter stuff"
 WINDOW_3="logs"
 
 tmux has-session -t $SESSIONNAME &> /dev/null
@@ -18,27 +18,28 @@ if [ $? != 0 ]
 
 	 # first window 
 	 tmux rename-window -t 1 $WINDOW_1 
-         tmux send-keys -t $WINDOW_1 'cd ~' C-m
-         tmux send-keys -t $WINDOW_1 'clear' C-m
-	 tmux split-window -h
+         #tmux send-keys -t $WINDOW_1 'cd ~' C-m
+         #tmux send-keys -t $WINDOW_1 'clear' C-m
+	 #tmux split-window -h
 	 #tmux send-keys -t $WINDOW_1 'htop' C-m
-	 tmux split-window -v
-	 tmux send-keys -t $WINDOW_1 'cd ~/workspace' C-m
-         tmux send-keys -t $WINDOW_1 'clear' C-m
-	 tmux send-keys -t $WINDOW_1 ll C-m
+	 #tmux split-window -v
+	 #tmux send-keys -t $WINDOW_1 'cd ~/workspace' C-m
+         #tmux send-keys -t $WINDOW_1 'clear' C-m
+	 #tmux send-keys -t $WINDOW_1 ll C-m
 
 	 # second window
 	 tmux new-window -t $SESSIONNAME:2 -n $WINDOW_2
-	 tmux split-window -h
+	 #tmux split-window -h
 
 
 	 # third window
-	 tmux new-window -t $SESSIONNAME:3 -n $WINDOW_3
-	 tmux split-window -h
-	 tmux split-window -v
+	 #tmux new-window -t $SESSIONNAME:3 -n $WINDOW_3
+	 #tmux split-window -h
+	 #tmux split-window -v
 
 	 # Switch to first window
-	 tmux select-window -t $SESSIONNAME:1.left
+	 tmux select-window -t $SESSIONNAME:1
+	 tmux select-pane -t 0
 fi
 
 
